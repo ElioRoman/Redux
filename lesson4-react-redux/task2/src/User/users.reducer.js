6547fa97a12e54ee98d0dad0a4078b9d192ffa47
ElioRoman <kolomiets.roman30@gmail.com>
2021-02-09 23:57:19 +0200

    add file

---
+++ lesson4-react-redux/task2/src/User/users.reducer.js
@@ -0,0 +1,40 @@
+import { ADD_USER, DELETE_USER, UPDATE_USER } from './users.actions';
+
+const initialState = {
+  usersList: [],
+};
+
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_USER: {
+      return {
+        ...state,
+        usersList: state.usersList.concat(action.payload.newUser),
+      };
+    }
+    case DELETE_USER: {
+      return {
+        ...state,
+        usersList: state.usersList.filter(user => user.id !== action.payload.id),
+      };
+    }
+    case UPDATE_USER: {
+      return {
+        ...state,
+        usersList: state.usersList.map(user => {
+          if (user.id === action.payload.id) {
+            return {
+              ...user,
+              ...action.payload.userData,
+            };
+          }
+          return user;
+        }),
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
