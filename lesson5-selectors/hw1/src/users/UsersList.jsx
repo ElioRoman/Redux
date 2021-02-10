d60cf83495f4007f84bb98b89c20b594b9f88097
ElioRoman <kolomiets.roman30@gmail.com>
2021-02-11 00:01:48 +0200

    add file

---
+++ lesson5-selectors/hw1/src/users/UsersList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import User from './User.jsx';
+import Filter from '../Filter.jsx';
+import { setFilterText } from './user.actions';
+import { filteredUsersSelector, filterTextSelector } from './users.selectors';
+
+const UsersList = ({ usersList, filterText, handleChange }) => {
+  return (
+    <div>
+      <Filter
+        text={filterText}
+        count={usersList.length}
+        onChange={e => handleChange(e.target.value)}
+      />
+      <ul className="users">
+        {usersList.map(user => {
+          return <User key={user.id} {...user} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+const mapState = state => {
+  return {
+    usersList: filteredUsersSelector(state),
+    filterText: filterTextSelector(state),
+  };
+};
+
+const mapDispatch = {
+  handleChange: setFilterText,
+};
+export default connect(mapState, mapDispatch)(UsersList);
