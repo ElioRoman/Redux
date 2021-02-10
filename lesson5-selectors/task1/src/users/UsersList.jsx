ecabe1e67af30df067808934d65d77dde1b5206a
ElioRoman <kolomiets.roman30@gmail.com>
2021-02-10 15:19:33 +0200

    add file

---
+++ lesson5-selectors/task1/src/users/UsersList.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import User from './User.jsx';
+import Pagination from './Pagination';
+import * as usersActions from './users.actions';
+import { currentPageSelector, usersListSelector } from './users.selectors';
+
+const UsersList = ({ users, prevPage, nextPage }) => {
+  const usersPerPage = 3;
+  const start = users.currentPage * usersPerPage;
+
+  return (
+    <div>
+      <Pagination
+        goPrev={prevPage}
+        goNext={nextPage}
+        currentPage={users.currentPage}
+        totalItems={users.usersList.length}
+        itemsPerPage={usersPerPage}
+      />
+      <ul className="users">
+        {users.usersList.slice(start, start + usersPerPage).map(user => (
+          <User name={user.name} age={user.age} key={user.id} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const mapState = state => {
+  return {
+    users: {
+      usersList: usersListSelector(state),
+      currentPage: currentPageSelector(state),
+    },
+  };
+};
+
+const mapDispatch = {
+  nextPage: usersActions.nextPage,
+  prevPage: usersActions.prevPage,
+};
+
+const connector = connect(mapState, mapDispatch);
+
+export default connector(UsersList);
