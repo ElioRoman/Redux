838d3e90704459c35ec17312714012f0eefb6d1c
ElioRoman <kolomiets.roman30@gmail.com>
2021-02-10 14:18:59 +0200

    add file

---
+++ lesson4-react-redux/hw1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import UsersList from './users/UsersList';
+import store from './store';
+
+const App = () => {
+  return (
+    <ul className="users">
+      <Provider store={store}>
+        <UsersList />
+      </Provider>
+    </ul>
+  );
+};
+
+export default App;
