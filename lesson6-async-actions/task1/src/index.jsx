26684c5e84baeb7e93a92a64eaf5c6e29b020ce2
ElioRoman <kolomiets.roman30@gmail.com>
2021-02-11 12:10:20 +0200

    add file

---
+++ lesson6-async-actions/task1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+
+const elem = document.querySelector('#root');
+
+ReactDOM.render(<App />, elem);
