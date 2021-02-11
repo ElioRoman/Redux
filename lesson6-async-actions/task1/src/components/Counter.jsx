ae383e53c383675a4d69beb5f00a928a59f8649e
ElioRoman <kolomiets.roman30@gmail.com>
2021-02-11 12:19:56 +0200

    add file

--- lesson6-async-actions/task1/src/components/Counter.jsx
+++ lesson6-async-actions/task1/src/components/Counter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import * as counterActions from '../counter.actions';
+import * as counterActions from './counter.actions';
 
 const Counter = ({ counter, increment, decrement, reset }) => {
   return (
@@ -18,18 +18,18 @@ const Counter = ({ counter, increment, decrement, reset }) => {
   );
 };
 
-const mapDispatch = {
-  increment: counterActions.increment,
-  decrement: counterActions.decrement,
-  reset: counterActions.reset,
-};
-
 const mapState = state => {
   return {
     counter: state,
   };
 };
 
-const connector = connect(mapState, mapDispatch); //HOC
+const mapDispatch = {
+  increment: counterActions.increment,
+  decrement: counterActions.decrement,
+  reset: counterActions.reset,
+};
+
+const connector = connect(mapState, mapDispatch);
 
 export default connector(Counter);
