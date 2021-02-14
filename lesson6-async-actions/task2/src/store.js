79f52b31b090686f057e41e33218b29aeaaed67e
ElioRoman <kolomiets.roman30@gmail.com>
2021-02-14 16:09:38 +0200

    add file

--- lesson6-async-actions/task2/src/store.js
+++ lesson6-async-actions/task2/src/store.js
@@ -1,13 +1,22 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import usersReducer from './users/users.reducer';
+import userReducer from './users/users.reducer';
 
-const appReducer = combineReducers({
-  users: usersReducer,
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('daispatching', action);
+  const result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+const reducer = combineReducers({
+  users: userReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(appReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 export default store;
